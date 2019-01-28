c1aae2c971e337bbb097f26fdeae449abc05a301
Artem <artem@webart-tech.ru>
2019-01-28 05:16:53 +0300

    add curve draving (not done)

--- primitives/pencil/pencil.go
+++ primitives/pencil/pencil.go
@@ -43,9 +43,34 @@ func (p *Pencil) SetColor(cr *cairo.Context) {
 }
 
 func (p *Pencil) Draw(cr *cairo.Context) {
-	for _, point := range p.points {
-		log.Println(point[0], point[1])
-		cr.Rectangle(point[0], point[1], 1, 1)
-		cr.Fill()
+	log.Println(p.points, "pdpdpdp")
+	cr.SetLineWidth(0.5)
+	cr.Scale(1, 1);
+	cr.MoveTo(p.points[0][0], p.points[0][1]);
+	if len(p.points) < 3 {
+		return
 	}
+	for i := 1;  i < len(p.points); i += 3 {
+		if i == 0 {
+			continue
+		}
+		if len(p.points) < i-1 {
+			break
+		}
+		if len(p.points) < i +1 {
+			break
+		}
+		if len(p.points) <= i +2 {
+			break
+		}
+		log.Println("i0", p.points[i], i, len(p.points))
+		log.Println("i1+1", p.points[i+1])
+		log.Println("i2+2", p.points[i+2])
+		cr.CurveTo(p.points[i][0], p.points[i][1], p.points[i+1][0], p.points[i+1][1], p.points[i+2][0], p.points[i+2][1])
+		log.Println(p.points[i])
+		//cr.Rectangle(point[0], point[1], 1, 1)
+		//cr.Fill()
+	}
+
+	cr.Stroke()
 }
